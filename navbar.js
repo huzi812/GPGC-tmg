a5df8d478016d9d56c61ec9303c8569cfb580e94
Huzaifa habib <huzimoon812@gmail.com>
2021-08-01 15:37:52 +0500

    updating

--- navbar.js
+++ navbar.js
@@ -1,2 +1,2 @@
-var navList=document.getElementById("nav-lists");function Show(){navList.classList.add("_Menus-show")}
-function Hide(){navList.classList.remove("_Menus-show")}
+var navList = document.getElementById("nav-lists"); function Show() { navList.classList.add("_Menus-show") }
+function Hide() { navList.classList.remove("_Menus-show") }
